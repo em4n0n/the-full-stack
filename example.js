125ead486d40377d0cb362d9bd941cf9e01f3b38
em4n0n <58372945+em4n0n@users.noreply.github.com>
2025-02-09 20:18:31 -0800

    append h2 element to the DOM

--- example.js
+++ example.js
@@ -3,3 +3,4 @@ const h2 = document.createElement('h2')
 h2.innerText = 'This is is an h2 heading'
 h2.setAttribute('id', 'sub-heading')
 h2.setAttribute('class', 'secondary')
+document.body.appendChild(h2)
